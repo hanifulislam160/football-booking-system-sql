1070c1667662d700c65239b42fcea93495e5ab3c
Haniful Islam <hanifulislam160@gmail.com>
2026-06-14 20:27:58 +0600

    all query add

--- QUERY.sql
+++ QUERY.sql
@@ -52,6 +52,62 @@ WHERE tournament_category = 'Champions League'
 AND match_status = 'Available';
 
 
+-- Query two
+SELECT user_id, full_name, email
+FROM Users
+WHERE full_name ILIKE 'Tanvir%' 
+   OR full_name ILIKE '%Haque%';
+
+
+-- Query three
+SELECT 
+    booking_id, 
+    user_id, 
+    match_id, 
+    COALESCE(payment_status::VARCHAR, 'Action Required') AS systematic_status
+FROM Bookings
+WHERE payment_status IS NULL;
+
+-- Query four
+SELECT 
+    b.booking_id, 
+    u.full_name, 
+    m.fixture, 
+    b.total_cost
+FROM Bookings b
+INNER JOIN Users u ON b.user_id = u.user_id
+INNER JOIN Matches m ON b.match_id = m.match_id;
+
+-- Query five
+SELECT 
+    u.user_id, 
+    u.full_name, 
+    b.booking_id
+FROM Users u
+LEFT JOIN Bookings b ON u.user_id = b.user_id
+ORDER BY u.user_id ASC;
+
+-- Query six
+SELECT 
+    booking_id, 
+    match_id, 
+    total_cost
+FROM Bookings
+WHERE total_cost > (SELECT AVG(total_cost) FROM Bookings)
+ORDER BY booking_id ASC;
+
+-- Query seven
+SELECT 
+    match_id, 
+    fixture, 
+    base_ticket_price
+FROM Matches
+ORDER BY base_ticket_price DESC
+LIMIT 2 OFFSET 1;
+
+
+
+
 INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
 (1, 'Haniful Islam', 'haniful@gmail.com', 'Football Fan', '01711111111'),
 (2, 'Antor Ahmed', 'antor@gmail.com', 'Football Fan', '01722222222'),
